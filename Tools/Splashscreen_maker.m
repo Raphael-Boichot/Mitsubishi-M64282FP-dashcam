e8a4654f65621128f30940590fa62a9c55747f98
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2023-02-05 23:33:34 +0100

    Add files via upload

---
+++ Tools/Splashscreen_maker.m
@@ -0,0 +1,20 @@
+a=imread('splash.png');
+
+fid=fopen('Hex_data.txt','w');
+[hauteur, largeur, pro]=size(a);
+counter=0;
+
+fprintf(fid,'const unsigned int splashscreen[128 * 160] = {');
+for i=1:1:hauteur
+   for j=1:1:largeur
+       counter=counter+1;
+       fprintf(fid,'0x');
+       if a(i,j)<=0xF; fprintf(fid,'0');end;
+      fprintf(fid,'%X',a(i,j));
+      fprintf(fid,',');
+      if rem(counter,16)==0;fprintf(fid,'\n'); end;
+   end
+end
+fseek(fid,-2,'cof');
+fprintf(fid,'};');
+fclose(fid);
