a38e14fe7748a021edc70e44e627a845a6eb1d4a
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2023-04-16 16:39:01 +0200

    New calibration

--- 240x240 SMC version/240x240_Dashboy_Camera/config.h
+++ 240x240 SMC version/240x240_Dashboy_Camera/config.h
@@ -111,7 +111,7 @@ unsigned char camReg4[8] = {0b10101111, 0b11101000, 0b00000000, 0b00000000, 0b00
 //transition@ C=0x8500
 unsigned char camReg5[8] = {0b10100111, 0b00001010, 0b00000000, 0b00000000, 0b00000001, 0b00000000, 0b00000001, 0b00000011};//high exposure time - low light
 //It is assumed that usefull range is between 1.5 and 3.0 volts, so between 116 and 232
-unsigned char GB_v_min = 120; //minimal voltage returned by the sensor in 8 bits DEC (1.5 volts is 112 but 120 gives better black)
+unsigned char GB_v_min = 130; //minimal voltage returned by the sensor in 8 bits DEC (1.5 volts is 112 but 120 gives better black)
 unsigned char GB_v_max = 210;//maximal voltage returned by the sensor in 8 bits DEC (3.05 volts is 236 but 220 gives better white)
 /////////////////////////
 
