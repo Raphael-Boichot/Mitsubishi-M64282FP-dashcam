73319adc338d43c6771d3b390de9a69a7c5caf64
Raphaël BOICHOT <raphael.boichot@gmail.com>
2025-04-22 16:57:40 +0200

    Update Gif_Maker_from_png_data.m

--- SD/GNU Octave only/Gif_Maker_from_png_data.m
+++ SD/GNU Octave only/Gif_Maker_from_png_data.m
@@ -1,19 +1,21 @@
 clc
 clear
-pkg load image
 %script to be run directly in the image folder
+
+disp('-----------------------------------------------------------')
+disp('|Beware, this code is for GNU Octave ONLY !!!             |')
+disp('-----------------------------------------------------------')
+pkg load image
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %User parameters
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%target_mp4_file='Output.mp4'; %target file for mp4, keep all image
 target_gif_file='Output.gif'; %target file for animated gif
 gif_deadtime=0.05;            %delay is seconds between pictures for animated gifs
 gif_skip=2;                   %keep every 1 out of gif_skip image for gif
 scaling_factor=0.5;           %because images are 8x after powershell step
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%vidfile = VideoWriter(target_mp4_file,'MPEG-4');
-%open(vidfile);
 listing = dir('*.png');
 for i=1:1:length(listing)
     name=listing(i).name;
@@ -21,16 +23,13 @@ for i=1:1:length(listing)
     frame=imread(name);
     [height, width, null]=size(frame);
     frame=imresize(frame,scaling_factor,'nearest');
-    %writeVideo(vidfile, frame);
-    %[imind,map] = rgb2ind(cat(3,frame),256); %This is the Matlan command
     [imind,map] = rgb2ind(cat(3,frame));
     if i==1
-        imwrite(imind,map,target_gif_file,'gif', 'Loopcount',inf,'DelayTime',gif_deadtime);
+        imwrite(imind,map,target_gif_file,'gif', 'Loopcount',inf,'DelayTime',gif_deadtime,'Compression','bzip');
     else
         if rem(i,gif_skip)==0
-        imwrite(imind,map,target_gif_file,'gif','WriteMode','append','DelayTime',gif_deadtime);
+        imwrite(imind,map,target_gif_file,'gif','WriteMode','append','DelayTime',gif_deadtime,'Compression','bzip');
         end
     end
 end
-%close(vidfile)
 disp('End of conversion, enjoy your fancy animations !')
